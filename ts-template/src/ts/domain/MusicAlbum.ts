d98dad6c2f189df2b357f9cb865fa970bab17343
AzaElvira <121398338+AzaElvira@users.noreply.github.com>
2023-09-02 23:26:24 +0400

    Create MusicAlbum.ts

---
+++ ts-template/src/ts/domain/MusicAlbum.ts
@@ -0,0 +1,10 @@
+mport Buyable from './Buyable';
+
+export default class MusicAlbum implements Buyable {
+    constructor(
+        readonly id: number,
+        readonly name: string,
+        readonly author: string,
+        readonly price: number,
+    ) { }
+}
